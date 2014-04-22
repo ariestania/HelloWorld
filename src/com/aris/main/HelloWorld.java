c17fd23f4d0dd73b67a73144df71e9d130d5665e
Ariestania Winda <ariestaniabohay@gmail.com>
2014-04-22 15:35:26 +0700

    My first commit to github

---
+++ src/com/aris/main/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aris.main;
+
+/**
+ *
+ * @author ariestania.winda
+ */
+public class HelloWorld {
+    public static void main (String[] args){
+        TextPrint.doPrintText("Hi ..  I am Aris");
+    }
+}
---
+++ src/com/aris/main/TextPrint.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aris.main;
+
+/**
+ *
+ * @author ariestania.winda
+ */
+public class TextPrint {
+    public static void doPrintText(String text){
+        System.out.println(text);
+    }
+}
